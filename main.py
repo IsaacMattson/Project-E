2c589d6484b525ab1582ff51eb4dcdce4323d0ad
Isaac <mattsonisaac06@gmail.com>
2023-04-20 20:22:11 -0600

    20 04 c

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-
 from funcs import *
 from error import *
 
@@ -9,28 +8,17 @@ Atom   = (Symbol, Number)
 List   = list             
 Exp    = (Atom, List)        
 
-class T:
-    STRING = "STRING"
-    INT = "INT"
-    BOOL = "BOOL"
-    SYMBOL = "SYMBOL"
+class symbol():
+    def __init__(self, value):
+        self.value = value;
 
 class label(Exception): pass
     
-class Token:
-    def __init__(self,_type,value):
-        self.value = value
-        self._type = _type;
-
-    def __str__(self):
-        return f"<{self._type}:{self.value}>"
-    def __repr__(self):
-        return f"<{self._type}:{self.value}>"
 
 class Procedure:
 
     def __init__(self, parms, body, env):
-        self.parms = [parm.value for parm in parms]
+        self.parms = parms
         self.body = body
         self.env = env
 
@@ -45,10 +33,7 @@ class Environment(dict):
         self.update(zip(keys, values));
         self.parent = parent;
         
-    def find (self, key):
-
-
-        
+    def find (self, key):       
             return self if (key in self) else self.parent.find(key)
 
 
@@ -63,6 +48,7 @@ def native_env() -> Environment:
         'car':      lambda lst : lst[0],
         'cdr':      lambda lst : lst[1:],
         'load':     lambda t : solve(load_prgm(t))
+    
         
     })
     return env
@@ -76,66 +62,58 @@ def lex(text) -> str:
     else:
         return None, Error("?");
 
-def parse(program) -> list:  
+def parse(expr) -> list:  
     tokens = []
 
-    while len(program) != 0:
+    while len(expr) != 0:
 
-        word = program[0]
+        word = expr[0]
         if word == "(":
-            program.pop(0)
-            tokens.append(parse(program))
+            expr.pop(0)
+            tokens.append(parse(expr))
         elif word == ")":
-            program.pop(0)
+            expr.pop(0)
             return tokens
         elif word.isnumeric():
-            tokens.append(Token("INT", int(word)))
-            program.pop(0)
+            tokens.append(int(word))
+            expr.pop(0)
         elif word == "#t" or word == "#f":
-            tokens.append(Token("BOOL", word))
-            program.pop(0)
+            tokens.append(word)
+            expr.pop(0)
         else:
-            tokens.append(Token("SYMBOL", word))
-            program.pop(0)
+            tokens.append(word)
+            expr.pop(0)
     return tokens
 
 def crawl(prgm):
-    
+    pass
 
-def eval(program: (Token, list), env = global_env):
-    if isinstance(program, Token):
-        
-        if program._type == 'SYMBOL':
-            return env.find(program.value)[program.value]
-        elif program._type == 'INT' or program._type == 'BOOL':
-            return program.value
-    elif  isinstance(program[0], list) :
-        return eval(program[0], env)
-    elif program[0]._type == T.INT or program[0]._type == T.BOOL:
-        return [eval(arg, env) for arg in program]
-    
+def eval(expr , env = global_env):
+    if isinstance(expr, list) == False:
+        if isinstance(expr, str):
+            return env.find(expr)[expr]
+        else:
+            return expr
     else:
         
-        op = program[0].value
-        args = program[1:]
-    
-        if op == "lambda":
-            return Procedure(program[1], program[2], env)
+        op = expr[0]
+        args = expr[1:]
+        if isinstance(op, int):
+            return expr
+        elif op == "lambda":
+            return Procedure(expr[1], expr[2], env)
         elif op == "define":
-            env[program[1].value] = eval(program[2], env)
+            env[args[0]] = eval(args[1], env)
         elif op == "if":
             if eval(args[0], env) == "#t":
                 return eval(args[1], env)
             else: return eval(args[2], env)
         elif op == "quote":
-            arg = args[0]
-            if isinstance(arg, Token):
-                return arg.value
-            
-                
+                return args[0]
         else:        
-            procedure = eval(program[0], env)
-            values = [eval(arg, env) for arg in program[1:]]
+            procedure = eval(expr[0], env)
+            values = [eval(arg, env) for arg in expr[1:]]
+            print(f"### op is '{procedure}', args are '{values}'")
             return procedure(*values)
 
 def load_prgm(fileName):
@@ -146,24 +124,27 @@ def load_prgm(fileName):
     return file.read()
 
 def solve(text):
-    error = None
-    
+
     try:
-        if isinstance(text, Error): error = text; raise label
-        lexed,error = lex(text)
-        if error != None: raise label
-        parsed = parse(lexed)
-        result = eval(parsed)
-    except label:
-        return error
-    return result
+        error = None
         
+        try:
+            if isinstance(text, Error): error = text; raise label
+            lexed,error = lex(text)
+            if error != None: raise label
+            parsed = parse(lexed)[0]
+            result = eval(parsed)
+        except label:
+            return error
+        return result
+    except: 
+        return '?.' # The experienced user will know what is wrong. 
 
 def repl():
 
     _input = ""
 
-    print(solve(load_prgm("test.ls")))
+##    print(solve(load_prgm("test.ls")))
     while True:
         _input = input("Geoff>")
         if _input == "!QUIT":
